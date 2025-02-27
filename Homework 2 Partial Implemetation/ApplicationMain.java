b66bdcda44cc2987a2b86cfd790ad704f93fc5db
Ayşegül Orhon <aysegul.orhon@ug.bilkent.edu.tr>
2025-02-28 00:10:34 +0300

    added option of 4 computer players

--- Homework 2 Partial Implemetation/ApplicationMain.java
+++ Homework 2 Partial Implemetation/ApplicationMain.java
@@ -6,10 +6,29 @@ public class ApplicationMain {
         Scanner sc = new Scanner(System.in);
         OkeyGame game = new OkeyGame();
 
-        System.out.print("Please enter your name: ");
-        String playerName = sc.next();
+        int choice = 0;
+        do{
+            System.out.println("Choose playing option:\n1-With 3 computer players\n2- 4 computer players");
+            if(sc.hasNextInt())
+            {
+                choice = sc.nextInt(); sc.nextLine();
+            }
+            else
+            {
+                sc.nextLine();
+            }
+        }while(choice < 1 || choice > 2);
 
-        game.setPlayerName(0, playerName);
+        if(choice == 1)//3 computer, 1 user player option
+        {
+            System.out.print("Please enter your name: ");
+            String playerName = sc.next();
+            game.setPlayerName(0, playerName);
+        }
+        else//4 computer players option
+        {
+            game.setPlayerName(0, "Alan");
+        }
         game.setPlayerName(1, "John");
         game.setPlayerName(2, "Jane");
         game.setPlayerName(3, "Ted");
@@ -32,8 +51,8 @@ public class ApplicationMain {
             int currentPlayer = game.getCurrentPlayerIndex();
             System.out.println(game.getCurrentPlayerName() + "'s turn.");
             
-            if(currentPlayer == 0) {
-                // this is the human player's turn
+            if(choice == 1 && currentPlayer == 0) {
+                // this is the human player's turn (when it is the 3 computer players version meaning choice was 1)
                 game.displayCurrentPlayersTiles();
                 game.displayDiscardInformation();
 
